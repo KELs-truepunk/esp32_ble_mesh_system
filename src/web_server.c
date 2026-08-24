cb92008c5c9796cfb35ae7a7f9598f3530094b4a
kels <ivan.bogdanov271210@gmail.com>
2026-08-24 16:46:20 +0500

    asmbling index.html in ram

--- src/web_server.c
+++ src/web_server.c
@@ -1,28 +1,26 @@
 #include "esp_http_server.h"
 #include "ble_mesh_transport.h"
 #include "esp_log.h"
-
+#include "index_html_autogen.h"
 static const char *TAG = "WEB_SERVER";
 static httpd_handle_t server = NULL;
 
-// Зашиваем index.html прямо в бинарник через сборщик CMake
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[]   asm("_binary_index_html_end");
 
 static esp_err_t root_get_handler(httpd_req_t *req)
 {
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, (const char *)index_html_start, index_html_end - index_html_start);
+    // sizeof(index_html) - 1, чтобы не отправлять терминирующий \0
+    httpd_resp_send(req, index_html, sizeof(index_html) - 1);
+    return ESP_OK;
 }
-
 static esp_err_t get_messages_handler(httpd_req_t *req)
 {
     mesh_web_msg_t msg;
     httpd_resp_set_type(req, "application/json");
 
-    if (mesh_pop_web_message(&msg)) {
+    if (mesh_pop_web_message(&msg))
+    {
         char json[256];
-        snprintf(json, sizeof(json), "{\"sender\":\"0x%08X\",\"seq\":%d,\"text\":\"%s\"}", 
+        snprintf(json, sizeof(json), "{\"sender\":\"0x%08X\",\"seq\":%d,\"text\":\"%s\"}",
                  (unsigned int)msg.sender_id, msg.seq_num, msg.text);
         return httpd_resp_send(req, json, HTTPD_RESP_USE_STRLEN);
     }
@@ -33,7 +31,8 @@ static esp_err_t post_send_handler(httpd_req_t *req)
 {
     char buf[165] = {0};
     int ret = httpd_req_recv(req, buf, sizeof(buf) - 1);
-    if (ret <= 0) {
+    if (ret <= 0)
+    {
         httpd_resp_send_500(req);
         return ESP_FAIL;
     }
@@ -46,11 +45,12 @@ static esp_err_t post_send_handler(httpd_req_t *req)
 void start_web_server(void)
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    
-    if (httpd_start(&server, &config) == ESP_OK) {
-        httpd_uri_t uri_root = { .uri = "/", .method = HTTP_GET, .handler = root_get_handler };
-        httpd_uri_t uri_get  = { .uri = "/api/messages", .method = HTTP_GET, .handler = get_messages_handler };
-        httpd_uri_t uri_post = { .uri = "/api/send", .method = HTTP_POST, .handler = post_send_handler };
+
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
+        httpd_uri_t uri_root = {.uri = "/", .method = HTTP_GET, .handler = root_get_handler};
+        httpd_uri_t uri_get = {.uri = "/api/messages", .method = HTTP_GET, .handler = get_messages_handler};
+        httpd_uri_t uri_post = {.uri = "/api/send", .method = HTTP_POST, .handler = post_send_handler};
 
         httpd_register_uri_handler(server, &uri_root);
         httpd_register_uri_handler(server, &uri_get);
