6cc19855a17cf8a5869b4a68ba5185b55c4f18b5
kels <ivan.bogdanov271210@gmail.com>
2026-08-24 11:11:14 +0500

    wifi_init file

---
+++ src/wifi_app.c
@@ -0,0 +1,34 @@
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+
+static const char *TAG = "WIFI_AP";
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = "FloodingMesh_Node",
+            .ssid_len = strlen("FloodingMesh_Node"),
+            .channel = 6,
+            .password = "12345678",
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "SoftAP запущен. SSID: FloodingMesh_Node, IP: 192.168.4.1");
+}
